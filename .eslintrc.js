e56cddb8533e99c3cb2a6c312f880064e1c3afc0
vit <vit@flexbe.com>
2025-03-25 18:36:57 -0700

    eslint fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,8 +6,7 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 2020,
     sourceType: 'module',
-    project: './tsconfig.json',
-    tsconfigRootDir: __dirname,
+    project: './tsconfig.json'
   },
   rules: {
     '@typescript-eslint/explicit-function-return-type': 'warn',
