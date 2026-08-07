33d050b20a76acbc5edc9c8eab4646e10254a3b1
Maksim Kachurin <kachurun@gmail.com>
2026-08-07 16:27:12 -0300

    Add media client for site images and files.
    Support multipart uploads via postForm and mirror the new Nest media routes on SiteApi.

--- src/types/index.ts
+++ src/types/index.ts
@@ -3,6 +3,7 @@ export * from './animations';
 export * from './pages';
 export * from './stat';
 export * from './sandbox';
+export * from './media';
 
 export enum FlexbeAuthType {
     API_KEY = 'apiKey',
